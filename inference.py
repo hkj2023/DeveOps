8b6eb275b39c89d5eb574317714c8abd12d80548
OLLRP <OLLRP@MIS.llrp.gov>
2026-04-21 22:29:45 +0300

    Final data is added

--- inference.py
+++ inference.py
@@ -6,7 +6,7 @@ model = joblib.load("defect_prediction.pkl")
 
 # Load new data for inference
 # Example: if you want to predict on a CSV file with unseen data
-new_data = pd.read_csv("processed_data.csv")   # <-- replace with your actual file
+new_data = pd.read_csv("ML_Final_Final.csv")   # <-- replace with your actual file
 
 # Make predictions
 predictions = model.predict(new_data)
